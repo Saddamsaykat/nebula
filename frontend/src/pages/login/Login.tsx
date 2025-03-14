3146c4f7c8ff65f773209c0718244ed98519ccbc
Md. Saddam Hossain Saykat <mdshaikat965@gmail.com>
2025-03-14 16:49:41 +0600

    update

--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import bgImage from "../../assets/ZHSUST.jpg";
 import { Link } from "react-router-dom";
-
+import logoZhsust from "../../assets/FavIcon.jpg";
 const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
 
@@ -11,7 +11,12 @@ const Login = () => {
       style={{ backgroundImage: `url(${bgImage})` }}
     >
       <form className="relative bg-opacity-10 backdrop-blur-md border-2 border-white p-10 rounded-xl w-96">
-        <h1 className="text-center text-2xl font-medium text-white mb-6">Login</h1>
+        <Link to={'/home'} className="flex justify-center mb-2">
+          <img className="w-10 h-10 rounded-md" src={logoZhsust} alt="" />
+        </Link>
+        <h1 className="text-center text-2xl font-medium text-white mb-6">
+          Login
+        </h1>
 
         <div className="space-y-6">
           <div className="border-b-2 border-white flex items-center gap-3">
@@ -36,7 +41,9 @@ const Login = () => {
                 placeholder="Password"
               />
               <i
-                className={`ri-${showPassword ? "eye-line" : "eye-off-line"} absolute right-3 top-3 cursor-pointer text-white`}
+                className={`ri-${
+                  showPassword ? "eye-line" : "eye-off-line"
+                } absolute right-3 top-3 cursor-pointer text-white`}
                 onClick={() => setShowPassword(!showPassword)}
               ></i>
             </div>
@@ -48,13 +55,20 @@ const Login = () => {
             <input type="checkbox" className="w-4 h-4" />
             <label>Remember me</label>
           </div>
-          <a href="#" className="hover:underline">Forgot Password?</a>
+          <a href="#" className="hover:underline">
+            Forgot Password?
+          </a>
         </div>
 
-        <button className="w-full py-3 bg-white text-black font-medium rounded-lg mt-6">Login</button>
+        <button className="w-full py-3 bg-white text-black font-medium rounded-lg mt-6">
+          Login
+        </button>
 
         <p className="text-center text-white text-sm mt-4">
-          Don't have an account? <Link to={'/register'} className="font-medium hover:underline">Register</Link>
+          Don't have an account?{" "}
+          <Link to={"/register"} className="font-medium hover:underline">
+            Register
+          </Link>
         </p>
       </form>
     </div>
