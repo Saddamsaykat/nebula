a492624de5a9ef279050b2b5251aefebcc5bbce5
Md. Saddam Hossain Saykat <mdshaikat965@gmail.com>
2025-03-15 13:33:43 +0600

    update

--- frontend/src/firebase/firebase.ts
+++ frontend/src/firebase/firebase.ts
@@ -1,36 +1,14 @@
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_API_KEY,
-//   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-//   projectId: import.meta.env.VITE_PROJECT_ID,
-//   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-//   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-//   appId: import.meta.env.VITE_APP_ID,
-//   // measurementId: import.meta.env.VITE_MEASUREMENT_ID,
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const auth = getAuth(app);
-// export default auth;
-
-// Import the functions you need from the SDKs you need
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDsMdK9GBFmWow7uh5muG45c59ir6Lno54",
-  authDomain: "alumni-cbbc2.firebaseapp.com",
-  projectId: "alumni-cbbc2",
-  storageBucket: "alumni-cbbc2.firebasestorage.app",
-  messagingSenderId: "681405197231",
-  appId: "1:681405197231:web:195996f12e3be50b391d5a"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  // measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
