a46179253b6706350065e3076868599c9c9a2731
Md. Saddam Hossain Saykat <mdshaikat965@gmail.com>
2025-04-06 16:36:52 +0600

    update

--- frontend/src/api/baseUrl.ts
+++ frontend/src/api/baseUrl.ts
@@ -1,2 +1,2 @@
-export const baseUrl = import.meta.env.VITE__DEV_API_KEY || import.meta.env.VITE__DEV_API_LOCAL_KEY
+export const baseUrl = import.meta.env.VITE__DEV_API_KEY
 export const weatherApi = import.meta.env.VITE_WEATHER_API_KEY
