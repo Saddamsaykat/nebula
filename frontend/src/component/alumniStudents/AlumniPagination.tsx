5fe1630d5737d0c6e5cd190ad0f1225dde2ed929
Md. Saddam Hossain Saykat <90493307+Saddamsaykat@users.noreply.github.com>
2025-04-18 15:19:25 +0600

    update (#5)

--- frontend/src/component/alumniStudents/AlumniPagination.tsx
+++ frontend/src/component/alumniStudents/AlumniPagination.tsx
@@ -2,7 +2,6 @@
 const AlumniPagination = ({
   totalPages,
   setCurrentPage,
-  filteredData,
   currentPage,
 }: {
   totalPages: number;
@@ -10,34 +9,54 @@ const AlumniPagination = ({
   filteredData: any[];
   currentPage: number;
 }) => {
+  const handlePrevious = () => {
+    if (currentPage > 0) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (currentPage < totalPages - 1) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePageClick = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <div>
       {totalPages > 1 && (
-        <div className="flex flex-wrap justify-center gap-2 mt-8">
+        <div className="flex items-center flex-wrap justify-center mt-8 space-x-4">
           <button
-            onClick={() => setCurrentPage(Math.max(currentPage - 1, 0))}
+            onClick={handlePrevious}
+            disabled={currentPage === 0}
+            className="px-4 py-1 rounded-full bg-gray-200 text-gray-700 hover:bg-[#3B9DF8] hover:text-white disabled:cursor-not-allowed disabled:opacity-50 disabled:hover:bg-gray-200 disabled:hover:text-gray-700 transition-all duration-300"
           >
             Previous
           </button>
 
-          {filteredData.map((_, index) => (
-            <button
-              key={index}
-              onClick={() => setCurrentPage(index)}
-              className={`px-3 py-1 rounded ${
-                currentPage === index
-                  ? "bg-blue-500 text-white"
-                  : "bg-gray-200 text-gray-800 hover:bg-gray-300"
-              }`}
-            >
-              {index + 1}
-            </button>
-          ))}
+          <div className="flex items-center flex-wrap justify-center gap-2">
+            {Array.from({ length: totalPages }, (_, index) => (
+              <button
+                key={index}
+                onClick={() => handlePageClick(index)}
+                className={`mx-1 px-3 py-1 text-[0.9rem] sm:text-[1rem] rounded-full bg-gray-200 text-gray-700 hover:bg-gray-300 transition-all duration-300 transform hover:scale-105 ${
+                  currentPage === index
+                    ? "!bg-[#3B9DF8] !text-white shadow-lg"
+                    : ""
+                }`}
+              >
+                {index + 1}
+              </button>
+            ))}
+          </div>
 
           <button
-            onClick={() =>
-              setCurrentPage(Math.min(currentPage + 1, totalPages - 1))
-            }
+            onClick={handleNext}
+            disabled={currentPage === totalPages - 1}
+            className="px-4 py-1 rounded-full bg-gray-200 text-gray-700 hover:bg-[#3B9DF8] hover:text-white disabled:cursor-not-allowed disabled:opacity-50 disabled:hover:bg-gray-200 disabled:hover:text-gray-700 transition-all duration-300"
           >
             Next
           </button>
